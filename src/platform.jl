886438d6ddef546a495e079a7154799434d7e29f
Gnimuc <qiyupei@gmail.com>
2021-05-29 13:05:13 +0900

    Fix a dpi scale issue for multi-viewport

--- src/platform.jl
+++ src/platform.jl
@@ -1,10 +1,16 @@
 function ImGui_ImplOpenGL3_RenderWindow(viewport::Ptr{ImGuiViewport}, gl_ctx_ptr::Ptr{Cvoid})::Cvoid
-    vp = unsafe_load(viewport)
-    if !(vp.Flags & ImGuiViewportFlags_NoRendererClear == ImGuiViewportFlags_NoRendererClear)
+    if !(unsafe_load(viewport.Flags) & ImGuiViewportFlags_NoRendererClear == ImGuiViewportFlags_NoRendererClear)
         glClearColor(0.0f0, 0.0f0, 0.0f0, 1.0f0)
         glClear(GL_COLOR_BUFFER_BIT)
     end
-    ImGui_ImplOpenGL3_RenderDrawData(unsafe_pointer_to_objref(gl_ctx_ptr), vp.DrawData)
+    # TODO: submit this to upstream
+    dpi_scale = unsafe_load(viewport.DpiScale)
+    draw_data = unsafe_load(viewport.DrawData)
+    if unsafe_load(draw_data.FramebufferScale.x) != dpi_scale
+        draw_data.FramebufferScale.x = dpi_scale
+        draw_data.FramebufferScale.y = dpi_scale
+    end
+    ImGui_ImplOpenGL3_RenderDrawData(unsafe_pointer_to_objref(gl_ctx_ptr), draw_data)
     return nothing
 end
 
